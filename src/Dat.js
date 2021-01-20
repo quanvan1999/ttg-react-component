29fdc46a9d097559ecad2b141532b35429289c85
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-20 13:07:08 +0700

    pullcode

--- src/Dat.js
+++ src/Dat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
+import TableComponent from './components/Table/Table'
 import Container from './components/Container'
 import theme from './utils/theme'
 const title = {
@@ -13,6 +14,33 @@ export default function Dat(props) {
   return (
     <ThemeProvider theme = {theme.light}>
       <Container headline = {"Table Component"}>
+        <TableComponent>
+
+          <TableComponent.Header>
+            <TableComponent.Row>
+              <TableComponent.HeaderCell>{title.id}</TableComponent.HeaderCell>
+              <TableComponent.HeaderCell>{title.title}</TableComponent.HeaderCell>
+              <TableComponent.HeaderCell>{title.body}</TableComponent.HeaderCell>
+            </TableComponent.Row>
+          </TableComponent.Header>
+            
+          <TableComponent.Body>
+            {
+              data.map((value, index)=>{
+                return(
+                  <TableComponent.Row key={index}>
+                    <TableComponent.Cell>
+                      {value.id}
+                    </TableComponent.Cell>
+                    <TableComponent.Cell>{value.title}</TableComponent.Cell>
+                    <TableComponent.Cell>{value.body}</TableComponent.Cell>
+                  </TableComponent.Row>
+                )
+              })
+            }
+          </TableComponent.Body>
+
+        </TableComponent>
       </Container>
     </ThemeProvider>
   )

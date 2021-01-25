5cabaee5e26bf6d4c8b30b1a88433cd3d38df224
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-25 13:19:39 +0700

    b

--- src/Dat.js
+++ src/Dat.js
@@ -1,65 +1,34 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { ThemeProvider } from 'styled-components'
-import Table from './components/elements/Table'
-import Container from './components/Container'
-import theme from './utils/theme'
-import Pagination from './components/elements/Pagination'
+import React, {} from 'react'
+import {Container, ContainerFluid, Row, Col} from './layouts/Grid'
 
 export default function Dat() {
-  // const [data, setData] = useState([]);
-  // useEffect(()=>{
-  //   fetch('https://jsonplaceholder.typicode.com/posts')
-  //   .then(response => response.json())
-  //   .then(json => setData(json))
-  // },[])
-  // const [currentPage, setCurrentPage] = useState(1)
-  // const rowPerPage = useRef(10)
-  // const [activePage, setActive] = useState(1)
-
-  // const handleSetActive = (active)=>{
-  //   if(active !== "..."){
-  //     console.log(active)
-  //     setActive(active)
-  //   }
-  // }
-  const handleGetValue = (page)=>{
-    console.log(page)
-  }
 
   return (
-    <ThemeProvider theme = {theme.light}>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>ID</Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Age</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>Apple</Table.Cell>
-            <Table.Cell>10</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>2</Table.Cell>
-            <Table.Cell>Banana</Table.Cell>
-            <Table.Cell>11</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>3</Table.Cell>
-            <Table.Cell>Orange</Table.Cell>
-            <Table.Cell>12</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-      <Container headline = {"Table Component"}>
-        <Pagination totalPage={20} boundary={1} sibling={1}  handleGetValue={handleGetValue} activePage={1} />
+    <>
+      <Container>
+        <Row>
+          <Col col={2}>
+            Helllo World!
+          </Col>
+          <Col col={5}>
+            Hello Kitty
+          </Col>
+          <Col col={5}>
+            Hello Micky
+          </Col>
+          <Col col={7}>
+            Hi
+          </Col>
+          <Col col={12}>
+            Hi
+          </Col>
+        </Row>
       </Container>
-    </ThemeProvider>
+      <ContainerFluid></ContainerFluid>
+    </>
   )
 }
+
 
  
 

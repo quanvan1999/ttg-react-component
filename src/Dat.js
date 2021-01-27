2ef4a7d1e926bc11fceb9ca2e5ef0c47e80e5816
Quốc Đạt <65354747+Quoc-Dat1205@users.noreply.github.com>
2021-01-27 11:06:19 +0700

    update grid

--- src/Dat.js
+++ src/Dat.js
@@ -1,71 +1,102 @@
 import React, {} from 'react'
 import {Container, ContainerFluid, Row, Col} from './layouts/Grid'
 export default function Dat() {
-  // offSet,middle,auto
+  // offSet,middle,auto, 
+  //JustifiContent : center, left, flex-end....
   // sm, md, xs, lg, xl, xxl
   return (
     <>
       <Container>
-        <Row>
+        <Row auto>
           <Col col={3} offSet={4}>
-            Hello 1
+            offset-4
           </Col>
           <Col col={5}>
-            Hello 2
+            col-5
           </Col>
 
           <Col col={2}>
-            Hello 4
+            col-2
           </Col>
-          <Col col={2}>
-            Hello 4
+          <Col col={3}>
+            col-3
           </Col>
           <Col col={3} offSet={2}>
-            Hello 5
+            offset-2
           </Col>
 
-          <Col col={5} auto>
-            Hello 6
+          <Col col={5}>
+            col-5
           </Col>
         </Row>
 
         <Row>
           <Col col={8} middle sm={4}>
-            Hello world
+            col-middle
           </Col>
         </Row>
+
+        <Row JustifyContent="center">
+          <Col col={3}>
+            Justify center
+          </Col>
+          <Col col={3}>
+            Justify center
+          </Col>
+        </Row>
+
       </Container>
       <br/>
       <ContainerFluid>
         <Row>
           <Col col={3} offSet={4}>
-            Hello 1
+            offset-4
           </Col>
           <Col col={5}>
-            Hello 2
+            col-5
           </Col>
           
           <Col col={2}>
-            Hello 4
+            col-2
           </Col>
           <Col col={2}>
-            Hello 4
+            col-2
           </Col>
           <Col col={3} offSet={2}>
-            Hello 5
+            offset-2
           </Col>
 
           <Col col={5} auto>
-            Hello 6
+            text-auto
           </Col>
         </Row>
 
         <Row>
-          <Col col={8} middle sm={4}>
-            Hello world
+          <Col col={8} middle>
+            middle
+          </Col>
+        </Row>
+
+        <Row>
+          <Col auto>
+            text-auto middle
           </Col>
         </Row>
       </ContainerFluid>
+      <br/>
+      <Container>
+        <Row>
+          <Col>
+            default
+          </Col>
+          <Col>
+            default 1
+          </Col>
+          <Col>
+            default 2
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
